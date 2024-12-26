ae6cc060ff9af21efc6391d5d47ee5ee1047cb15
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2024-12-27 00:36:52 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -62,14 +62,24 @@ const loadFont = ( name ) => {
   // get data from fonts.json
   // apply font to <head>
 } 
-const getVariable = ( property, element ) => {
-  let el = element ? document.querySelector(element) : document.querySelector('html');
+const getVariable = ( property, selector ) => {
+  let el = selector ? document.querySelector(selector) : document.querySelector('html');
   var cs = getComputedStyle(el);
   return cs.getPropertyValue(property);
 } 
-const setVariable = ( property, value, element ) => {
-  let el = element ? document.querySelector(element) : document.querySelector('html');
+const setVariable = ( property, value, selector ) => {
+  let el = selector ? document.querySelector(selector) : document.querySelector('html');
   el.style.setProperty( property, value );
+}
+const getDataValue = ( name, selector ) => {
+  let el = selector ? document.querySelector(selector) : document.querySelector('html');
+  return el.getAttribute( 'data-' + name );
 } 
+const setDataValue = ( name, value, selector ) => {
+  let el = selector ? document.querySelector(selector) : document.querySelector('html');
+  el.setAttribute( 'data-' + name, value );
+} 
+
+
 
 
