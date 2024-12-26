9a5fa3b66ad999156c9f53edbbdd3204392386ad
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2024-12-26 23:37:13 +0100

    Create cookie.js

---
+++ js/cookie.js
@@ -0,0 +1,26 @@
+(function(){ "use strict";
+
+  this.cookie = ( name, value, days, domain ) => {
+  
+    if( !name.startsWith('!') && !value && !days ){
+  
+      // get cookie
+      let result = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+      return result ? result[2] : null;
+  
+    } else {
+  
+      // set values for removing cookie
+      name.startsWith('!') ? ( value = '', days = '-1' ) : '' ;
+  
+      // set cookie
+      let date = new Date;
+      date.setTime( date.getTime() + 24 * 60 * 60 * 1000 * ( days || 1000) );
+      let theDomain = domain ? ';domain=' + domain : '';
+      document.cookie = name + '=' + value + ';path=/;expires=' + date.toGMTString() + theDomain;
+  
+    }
+  
+  }
+
+}).call(this);
