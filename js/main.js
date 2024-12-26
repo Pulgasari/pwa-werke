740d572d84ddbc9edbe472978c3e53de52f4d868
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2024-12-26 22:19:24 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+
+function loadFile( filepath ){
+
+  console.log('function called: loadFile()');
+  console.log( filepath );
+  
+fetch( filepath )
+  .then(response => {
+    // When the page is loaded convert it to text
+    return response.text()
+  })
+  .then(html => {
+    console.log('Content of File successfully loaded.');
+    return html;
+  })
+  .catch(error => {
+     console.error('Failed to fetch page: ', error)
+  })
+
+} 
